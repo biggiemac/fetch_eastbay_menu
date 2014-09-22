ebcc6530b38524fd1659ee9af92aa827fefb628d
Aaron Gibson <agibson@adaptivecomputing.com>
2014-09-22 15:15:38 -0700

    adding fetch_easybay_menu.py

---
+++ fetch_eastbay_menu.py
@@ -0,0 +1,13 @@
+from lxml import html
+import requests
+
+# Page is the HTML page to parse, in this case we fetch eastbaycafe.com/menu.php
+page = requests.get('http://www.eastbaycafe.com/menu.php')
+
+# Now we form our tree variable and fill it with the pages HTML
+tree = html.fromstring(page.text)
+
+# Populate menu with the child div's from menu_content_today and iterate over them
+menu = tree.xpath('//div[@id="menu_content_today"]')[0]
+for item in menu.getchildren()[0].getchildren(0):
+  print item.text_content()
