880c4312488dc63a89d74b388fe9998445e7c660
Aaron Gibson <agibson@adaptivecomputing.com>
2014-09-22 15:24:03 -0700

    fixing syntax

--- fetch_eastbay_menu.py
+++ fetch_eastbay_menu.py
@@ -9,5 +9,5 @@ tree = html.fromstring(page.text)
 
 # Populate menu with the child div's from menu_content_today and iterate over them
 menu = tree.xpath('//div[@id="menu_content_today"]')[0]
-for item in menu.getchildren()[0].getchildren(0):
+for item in menu.getchildren()[0].getchildren():
   print item.text_content()
